22f354c5acebe7ab9855151397e12fc9570d96ad
NuBOX DevCom <slye@nubox.fr>
2019-01-27 18:11:05 +0100

    Added comments to file

--- dump.php
+++ dump.php
@@ -3,6 +3,11 @@
 use NDC\DatabaseBackup\System;
 
 require 'vendor/autoload.php';
-//$storage = new \League\Flysystem\AwsS3v3\AwsS3Adapter();
+/**
+ * Inject an FlysystemAdapter for use other file system (eg. AWS S3)
+ *
+ * $storage = new \League\Flysystem\AwsS3v3\AwsS3Adapter(*params*);
+ * $system = System::getInstance($storage);
+ */
 $system = System::getInstance();
-//$system->processBackup();
+$system->processBackup();
