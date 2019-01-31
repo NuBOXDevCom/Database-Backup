be6a75c873c42c51d36dd3c3f670cae7aa5ee445
NuBOX DevCom <slye@nubox.fr>
2019-02-01 00:56:34 +0100

    Added some comments

--- dump.php
+++ dump.php
@@ -7,7 +7,9 @@ require 'vendor/autoload.php';
  * Inject an FlysystemAdapter for use other file system (eg. AWS S3)
  *
  * $storage = new \League\Flysystem\AwsS3v3\AwsS3Adapter(*params*);
- * $system = System::getInstance($storage);
+ * $storageOptions = [];
+ * $system = System::getInstance($storage, $storageOptions);
  */
 $system = System::getInstance();
+// Available array options listed in https://github.com/ifsnop/mysqldump-php#dump-settings
 $system->processBackup();
